2990f530b842175d7c104d2bebb7d262467cdc44
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-07-01 11:04:18 +0200

    Printing list of characters from fecth

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import getCharacters from '../../service/rick-morty.service';
 
-export default function List() {
+function List() {
   const [characters, setCharacters] = useState([]);
   useEffect(() => {
-    getCharacters().then(setCharacters());
+    getCharacters().then(setCharacters);
   }, []);
   return (
     <>
-      {characters.map((character) => <li>{character}</li>)}
+      {characters.map((character) => (<li>{character.name}</li>))}
     </>
   );
 }
+
+export default List;
--- src/service/rick-morty.service.js
+++ src/service/rick-morty.service.js
@@ -1,7 +1,7 @@
 export default async function getCharacters() {
   try {
     const response = await fetch('https://rickandmortyapi.com/api/character');
-    const { results } = await response.json;
+    const { results } = await response.json();
     return results;
   } catch (error) {
     return error;
